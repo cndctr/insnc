f4ccf0d3cfa837c53aa0d2ee3739848a586d5b50
conductor <conductor85@gmail.com>
2025-08-07 15:26:15 +0300

    change version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="insnc",
-    version="0.6",
+    version="0.7",
     packages=find_packages(),
     install_requires=["requests", "openpyxl"],
     entry_points={
